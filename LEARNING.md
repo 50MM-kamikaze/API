3892620e98ec0b94e66b920221cdb5b67732fcf8
Samhith Beeravelli <samhith.wgl.in@gmail.com>
2023-07-05 17:11:34 +0530

    look in the lEARNING file to knowing more

---
+++ LEARNING.md
@@ -0,0 +1,16 @@
+different methods 
+
+Get --> Request data from a specified resource 
+Post --> Create a resource 
+Put --> Update a resource
+Delete --> delete a resource 
+
+query parameter "?"
+
+after the url like in the example;
+/get_user/123123?extra=hello world 
+
+run the following url in a browser after setting up the server in IDE 
+make sure to change the ip-address 
+
+use postman to check the working of your api's , cause we cant test our create_user in the IDE itself 
---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request , jsonify 
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "home"
+
+@app.route("/get_user/<user_id>") # lets say user_id is 876 , them we can take a value after the /get_user/876
+def get_user(user_id): # this is how we make a get route 
+    user_data = {
+        "user_id" : user_id,
+        "name" : "Samhith",
+        "email" : "samhith.wgl.in@gmail.com"
+    }
+    
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+        
+    return jsonify(user_data), 200
+
+
+@app.route("/create_user" , methods = ["POST"]) #unlike a get_user methods need to be mentioned 
+def create_user():
+    data = request.get_json()
+    return jsonify(data) , 201
+
+         
+
+if __name__ == "__main__":
+    app.run(debug=True)
